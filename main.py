858efdde60a1fa537990df6bc570470d54778b08
Samuel Salama <samy.salama@me.com>
2024-08-03 16:34:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from neuralintents import GenericAssistant
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import mplfinance
+
+import pickle
+import sys
+import datetime as dt
